c00b3d6db18d59d63e9f8d180251e936c736ace8
xukunluren <xukun@bilibili.com>
2018-05-22 21:56:47 +0800

    no message

--- pages/add/add.js
+++ pages/add/add.js
@@ -1,4 +1,4 @@
-
+const app = getApp()    
 Page({
 
   /**
@@ -15,7 +15,7 @@ Page({
   },
   imageTap :function(){
     wx.navigateTo({
-      url: '../textEdit/textEdit'
+      url: '../pictureEdit/pictureEdit'
     })
   },
   audioTap:function(){
@@ -37,6 +37,11 @@ Page({
           latitude: res.latitude,
           longitude: res.longitude
         })
+        app.globalData.latitude = res.latitude;
+        app.globalData.longitude = res.longitude;
+        console.log("---------------")
+        console.log(app.globalData.latitude)
+        console.log(app.globalData.longitude)
         console.log(res.latitude)
         console.log(res.longitude)
       }
@@ -47,7 +52,34 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    // 引入SDK核心类
+    var QQMapWX = require('../../utils/qqmap-wx-jssdk.js');
+
+    // 实例化API核心类
+    var demo = new QQMapWX({
+      key: 'Q3QBZ-VHVC4-CJ4U5-DZMSI-PJ7IJ-TZFOG' // 必填
+    });
+    var _this = this;
+    // 调用接口
+    demo.reverseGeocoder({
+      location: {
+        latitude: app.globalData.latitude,
+        longitude: app.globalData.longitude
+        // latitude: 39.984060,
+        // longitude: 116.307520
+      },
+      success: function (res) {
+        console.log(res);
+        app.globalData.address = res.result.address_component.city
+        console.log(_this.data.address)
+      },
+      fail: function (res) {
+        console.log(res);
+      },
+      complete: function (res) {
+        console.log(res);
+      }
+    });
   },
 
   /**
--- pages/pictureEdit/pictureEdit.js
+++ pages/pictureEdit/pictureEdit.js
@@ -1,4 +1,4 @@
-const app = getApp()    
+const app = getApp()
 
 Page({
 
@@ -6,10 +6,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    address:"",
-    latitude:"",
-    longitude:""
-    
+    address: "",
+    latitude: "",
+    longitude: "",
+    src : null
   },
 
   /**
@@ -19,60 +19,84 @@ Page({
     this.setData({
       address: app.globalData.address
     })
+    this.gotoShow()
+    
   },
+  gotoShow: function () {
+    var _this = this
+    wx.chooseImage({
+      count: 9, // 最多可以选择的图片张数，默认9
+      sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+      success: function (res) {
+        // success
+        console.log(res)
+        _this.setData({
+          src: res.tempFilePaths
+        })
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
     // 引入SDK核心类
-     
-   
-      
+
+
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   },
-  weatherTap:function(){
+  weatherTap: function () {
     wx.navigateTo({
       url: '../index/index',
     })
